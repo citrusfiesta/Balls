84334b767b58a4a81c4354d533744251df9b7189
citrusfiesta <citrusfiesta@gmail.com>
2015-04-01 17:49:16 +0200

    added defender

--- src/Level.ts
+++ src/Level.ts
@@ -1,5 +1,6 @@
 /// <reference path="libs/phaser.d.ts" />
 /// <reference path="Attacker.ts" />
+/// <reference path="Defender.ts" />
 /// <reference path="BallManager.ts" />
 /// <reference path="C.ts" />
 
@@ -8,20 +9,26 @@ module Balls {
 	export class Level extends Phaser.State {
 
 		private _attacker:Attacker;
+		private _defender:Defender;
 		private _ballMan:BallManager;
 
 		screenMid:number;
 
-		preload() {
-			this.load.image(C.ATTACKER, C.ATTACKER_PATH);
-			this.load.image(C.BALL, C.BALL_PATH);
-		}
+		//preload() {
+		//	this.load.image(C.ATTACKER, C.ATTACKER_PATH);
+		//	this.load.image(C.BALL, C.BALL_PATH);
+		//}
 
 		create() {
-			this._ballMan = new BallManager (this.game);
 			this.screenMid = this.game.stage.width / 2;
 			this.game.physics.enable(this.game);
-			this._attacker = new Attacker(this.game, this.screenMid, 500, C.ATTACKER, this._ballMan);
+
+			this._attacker = new Attacker(this.game, this.screenMid, 500, C.ATTACKER);
+			this._defender = new Defender(this.game, this.screenMid, 100, C.DEFENDER);
+			this._ballMan = new BallManager (this.game, [this._attacker, this._defender]);
+
+			// Pass the BallManger reference to the attacker.
+			this._attacker.setBallManager(this._ballMan);
 		}
 	}
 }
