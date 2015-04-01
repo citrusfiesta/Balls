773a87862e77395f422b26c5638c79df7ff07e31
citrusfiesta <citrusfiesta@gmail.com>
2015-04-01 17:47:18 +0200

    added internal pool
    To replace the (now) superfluous BallPool class. Its functionality fits
    nicely in BallManager

--- src/BallManager.ts
+++ src/BallManager.ts
@@ -10,7 +10,6 @@ module Balls {
 		 * Reference to the current instance of Phaser.Game
 		 */
 		private _game:Phaser.Game;
-		//private _ballPool:BallPool;
 		/**
 		 * The pool of Balls. Prevents constant instantiating and destruction.
 		 */
@@ -18,10 +17,9 @@ module Balls {
 
 		private _poolCounter:number;
 
-		constructor(game:Phaser.Game) {
+		constructor(game:Phaser.Game, collArray:Array<Phaser.Sprite>) {
 			this._game = game;
-			//this._ballPool = new BallPool(32, game, C.OFFSCREEN, C.OFFSCREEN, C.BALL, this);
-			this._pool = this._createPool(32, game, C.OFFSCREEN, C.OFFSCREEN, C.BALL);
+			this._pool = this._createPool(32, game, C.OFFSCREEN, C.OFFSCREEN, C.BALL, collArray);
 		}
 
 
@@ -33,6 +31,28 @@ module Balls {
 			this._returnBall(ball);
 		}
 
+		/**
+		 * Creates an array of the given size, fills it with instances of Ball, and returns that array.
+		 * @param size How large the array will be.
+		 * @param game The current instance of Phaser.Game. Used for creating an instance of Ball.
+		 * @param x	Horizontal spawn position when creating an instance of Ball.
+		 * @param y Vertical spawn position when creating an instance of Ball.
+		 * @param key Reference to the Ball texture. Used for creating an instance of Ball.
+		 * @param collArray Array of the items that instances of Ball can collide with.
+		 * @returns {Array<Ball>}
+		 * @private
+		 */
+		private _createPool(size:number, game:Phaser.Game, x:number, y:number, key:string,
+							collArray:Array<Phaser.Sprite>):Array<Ball> {
+			var array:Array<Ball> = [];
+			this._poolCounter = size;
+
+			var i:number = size;
+			while (--i > -1)
+				array.push(new Ball(game, x, y, key, this, collArray));
+			return array;
+		}
+
 		private _returnBall(ball:Ball):void {
 			this._pool[this._poolCounter++] = ball;
 		}
@@ -42,26 +62,6 @@ module Balls {
 				return this._pool[--this._poolCounter];
 			else
 				throw new Error ("Pool is exhausted.");
-		}
-
-		/**
-		 * Creates an array of the given size, fills it with instances of Ball, and returns that array.
-		 * @param size How large the array will be.
-		 * @param game The current instance of Phaser.Game. Used for creating an instance of Ball.
-		 * @param x	Horizontal spawn position when creating an instance of Ball.
-		 * @param y Vertical spawn position when creating an instance of Ball.
-		 * @param key Reference to the Ball texture. Used for creating an instance of Ball.
-		 * @returns {Array<Ball>}
-		 * @private
-		 */
-		private _createPool(size:number, game:Phaser.Game, x:number, y:number, key:string):Array<Ball> {
-			var array:Array<Ball> = [];
-			this._poolCounter = size;
-
-			var i:number = size;
-			while (--i > -1)
-				array.push(new Ball(game, x, y, key, this));
-			return array;
 		}
 	}
 }
