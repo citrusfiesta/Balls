879a8a157057d79a799c58c3c0f29c3e2d5bfb85
citrusfiesta <citrusfiesta@gmail.com>
2015-11-11 13:58:18 +0100

    move preload bar to middle

--- src/Preloader.ts
+++ src/Preloader.ts
@@ -3,33 +3,37 @@
 
 module Balls {
 
-	export class Preloader extends Phaser.State {
+    export class Preloader extends Phaser.State {
 
-		preloadBar:Phaser.Sprite;
+        preloadBar: Phaser.Sprite;
 
-		preload () {
+        preload() {
 
-			this.preloadBar = this.add.sprite(400, 300, C.PRELOAD_BAR);
-			this.load.setPreloadSprite(this.preloadBar);
+            this.preloadBar = this.add.sprite(400, 300, C.PRELOAD_BAR);
+            this.preloadBar.anchor.x = 0.5;
+            this.preloadBar.anchor.y = 0.5;
+            this.load.setPreloadSprite(this.preloadBar);
 
-			this.load.image(C.PLAY_BUTTON, C.PLAY_BUTTON_PATH);
-			this.load.image(C.ATTACKER, C.ATTACKER_PATH);
-			this.load.image(C.DEFENDER, C.DEFENDER_PATH);
-			this.load.image(C.BALL, C.BALL_PATH);
-			this.load.image(C.GROUND, C.GROUND_PATH);
-			this.load.image(C.GOAL, C.GOAL_PATH);
-		}
+            this.load.image(C.PLAY_BUTTON, C.PLAY_BUTTON_PATH);
+            this.load.image(C.ATTACKER, C.ATTACKER_PATH);
+            this.load.image(C.DEFENDER, C.DEFENDER_PATH);
+            this.load.image(C.BALL, C.BALL_PATH);
+            this.load.image(C.GROUND, C.GROUND_PATH);
+            this.load.image(C.GOAL, C.GOAL_PATH);
+        }
 
-		create () {
+        create() {
 
-			// Here you can do shit before calling startMainMenu ()
-			var tween = this.add.tween(this.preloadBar).to({alpha: 0}, 1000, Phaser.Easing.Linear.None, true);
-			tween.onComplete.add(this.startMainMenu, this);
-		}
+            // Here you can do shit before calling startMainMenu ()
+            var tween = this.add.tween(this.preloadBar).to({
+                alpha: 0
+            }, 1000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startMainMenu, this);
+        }
 
-		startMainMenu () {
+        startMainMenu() {
 
-			this.game.state.start (C.MAIN_MENU, true, false);
-		}
-	}
+            this.game.state.start(C.MAIN_MENU, true, false);
+        }
+    }
 }
