472336dc1515dd933215c506d11c000c2012e2f4
citrusfiesta <citrusfiesta@gmail.com>
2015-11-11 14:20:35 +0100

    add instructions to main menu

--- src/MainMenu.ts
+++ src/MainMenu.ts
@@ -12,6 +12,18 @@ module Balls {
                 this.game.stage.height / 2 + 100, C.PLAY_BUTTON, this.startGame, this);
             this.playButton.anchor.x = 0.5;
             this.playButton.anchor.y = 0.5;
+
+            var text: Phaser.Text = this.game.add.text(this.game.stage.width / 2, 20,
+                'Controls for square: left, right, up\nControls for rectangle: A, D\n\n' +
+                'Square shoots with UP and tries to score, rectangle defends by blocking\n\n' +
+                'Square should try to get as much points in 30 seconds as possible,\n' +
+                'rectangle should try to block as much as possible', {
+                    font: "25px sans-serif",
+                    fill: "#222222",
+                    align: 'center'
+                });
+            text.anchor.x = 0.5;
+            text.anchor.y = 0;
         }
 
         startGame() {
