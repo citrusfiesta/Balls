82ab4af3981c873a52d8d9175d14a6664b10f1c3
citrusfiesta <citrusfiesta@gmail.com>
2015-11-11 16:20:44 +0100

    fade to black after end

--- src/End.ts
+++ src/End.ts
@@ -1,11 +1,16 @@
 /// <reference path="libs/phaser.d.ts" />
+/// <reference path="C.ts" />
 
 module Balls {
 
-	export class End extends Phaser.State {
+    export class End extends Phaser.State {
 
-		create() {
-            console.log('end called');
-		}
-	}
+        create() {
+            var black = this.game.add.sprite(0, 0, C.BLACK);
+            black.alpha = 0;
+            this.add.tween(black).to({
+                alpha: 0.5
+            }, 1000, Phaser.Easing.Linear.None, true);
+        }
+    }
 }
