077dcf160a00cbec662a3fa7f1c138fad381b52b
citrusfiesta <citrusfiesta@gmail.com>
2015-11-11 16:44:07 +0100

    add text and restart function to end screen

--- src/End.ts
+++ src/End.ts
@@ -11,6 +11,22 @@ module Balls {
             this.add.tween(black).to({
                 alpha: 0.5
             }, 1000, Phaser.Easing.Linear.None, true);
+
+            var text: Phaser.Text = this.game.add.text(this.game.stage.width / 2, 150,
+                'Square ended the game with a score of ' + C.score.toString() + '\n\n' +
+                'Click anywhere to restart.', {
+                    font: "25px sans-serif",
+                    fill: "#dedede",
+                    align: 'center'
+                });
+            text.anchor.x = 0.5;
+            text.anchor.y = 0;
+
+            this.input.onDown.addOnce(this.restartGame, this);
+        }
+
+        restartGame() {
+            this.game.state.start(C.MAIN_MENU, true, false);
         }
     }
 }
